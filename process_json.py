7f572bd78cffba3b5aba715e148bee084b4497ef
mike dupont <mike.dupont@introspector.local>
2023-10-31 18:23:47 -0400

    update outputs

---
+++ process_json.py
@@ -0,0 +1,17 @@
+import os
+import subprocess
+
+files = [os.path.join(dirpath, filename)
+    for (dirpath, dirs, files) in os.walk('.')
+    for filename in (dirs + files)]
+for f  in files:
+    if f.endswith(".md"):
+        out = f + ".json"
+        if not os.path.exists(out):
+            dd = subprocess.run([
+                "pandoc", "-i", f,  "-o", out
+            ], capture_output=True)
+            if dd.stderr or dd.stdout:
+                print(f, dd.stderr,dd.stdout)
+            else:
+                print(out)
