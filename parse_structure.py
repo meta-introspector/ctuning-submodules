1683887c63b6165c1b7bcf77cf3d2ae3c6d71a64
mike dupont <mike.dupont@introspector.local>
2023-11-01 13:22:49 -0400

    updating from org mode

--- parse_structure.py
+++ parse_structure.py
@@ -19,7 +19,7 @@ last_stars    = 0
 last_content  = "start"
 
 lines = []
-with open("outline.txt") as fi:
+with open("data/outline.txt") as fi:
     for l in fi:
         l = l.strip()
         parts = l.split(":")
@@ -117,4 +117,70 @@ json_graph = json.dumps(graph_data)
 with open(f"graphs/graph.json", "w") as f:
     f.write(json_graph)
     nt.show(f"graphs/graph.html")
-        
+
+if True:
+    size = 30
+    dag = g.to_undirected()
+    #largest_components = sorted(nx.strongly_connected_components(dag), key=len, reverse=True)[:size]
+    largest_components = sorted(nx.connected_components(dag), key=len, reverse=True)[:size]
+    #print(len(largest_components))
+    for index,comp in enumerate(largest_components):
+        name= f'Component{index}'
+        component=dag.subgraph(comp)
+        nt = Network(notebook=True, width="100%", height="800px",
+                     #directed=True
+                     )
+        for edge in component.edges():
+            source_id_str = edge[0]
+            target_id_str = edge[1]
+            edge_id_str = (
+                f"{source_id_str}_to_{target_id_str}"  # Construct a unique edge id
+            )
+            nt.add_node(source_id_str)
+            nt.add_node(target_id_str)
+            nt.add_edge(source_id_str, target_id_str, id=edge_id_str)
+        hierarchical_options = {
+            "enabled": True,
+            #"levelSeparation": 200,  # Increased vertical spacing between levels
+            #"nodeSpacing": 250,  # Increased spacing between nodes on the same level
+            #"treeSpacing": 250,  # Increased spacing between different trees (for forest)
+            "blockShifting": True,
+           # "edgeMinimization": True,
+            "parentCentralization": True,
+            #"direction": "UD",
+            "sortMethod": "directed",
+        }
+        physics_options = {
+            "stabilization": {
+                "enabled": True,
+                "iterations": 1000,  # Default is often around 100
+            },
+            "hierarchicalRepulsion": {
+                "centralGravity": 0.0,
+                "springLength": 700,  # Increased edge length
+                "springConstant": 0.01,
+                "nodeDistance": 750,  # Increased minimum distance between nodes
+                "damping": 0.09,
+            },
+            "solver": "hierarchicalRepulsion",
+            "timestep": 0.5,
+        }
+        nt.options = {
+            "nodes": {
+                "font": {
+                    "size": 12,  # Increased font size for labels
+                    "color": "black",  # Set a readable font color
+                },
+                "shapeProperties": {"useBorderWithImage": True},
+            },
+            "edges": {
+                "length": 1050,  # Increased edge length
+            },
+            "physics": physics_options,
+            "layout": {"hierarchical": hierarchical_options},
+        }
+        graph_data = {"nodes": nt.nodes, "edges": nt.edges}
+        json_graph = json.dumps(graph_data)
+        with open(f"graphs/graph{name}.json", "w") as f:
+            f.write(json_graph)
+        nt.show(f"graphs/graph{name}.html")
